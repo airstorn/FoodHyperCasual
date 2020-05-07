669ca41b064b4e4ba8a9fb254efe4d346a79854a
Airstorn <mitichkinvladnov@gmail.com>
2020-05-07 16:24:14 +0700

    scene

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _raycastCamera;
+
+    public OnInputPressed InputPressedAction;
+    public delegate void OnInputPressed();
+    
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InputPressedAction?.Invoke();
+            RaycastHit hit;
+            if (Physics.Raycast(_raycastCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
